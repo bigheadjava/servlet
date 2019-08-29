de28d6025cb3898065ba68b7d8567bbdccb2c0c2
yinchu <javayinchu2013@163.com>
2019-08-29 22:08:35 +0800

    Servlet Loading

---
+++ src/main/java/com/cheer/load/MyServlet2.java
@@ -0,0 +1,27 @@
+package com.cheer.load;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet2 extends HttpServlet {
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("========== MyServlet2 加载完毕 =============");
+		super.init(config);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>MyServlet2.....</h1>");
+		
+	}
+}
