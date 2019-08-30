bd0d41050bf9215fe6b5d3ff8b0ea0df6a91a123
yinchu <javayinchu2013@163.com>
2019-08-30 17:15:30 +0800

    update

---
+++ src/main/java/com/cheer/annotation/AnnoServlet.java
@@ -0,0 +1,39 @@
+package com.cheer.annotation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "AnnoServlet", loadOnStartup = 1, urlPatterns = { "/AnnoServlet1", "/AnnoServlet2" }, initParams = {
+		@WebInitParam(name = "param1", value = "param1 value"),
+		@WebInitParam(name = "param2", value = "param2 value") })
+public class AnnoServlet extends HttpServlet {
+	ServletConfig cfg;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		cfg = config;
+		System.out.println("AnnoServlet初始化...");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		String p1 = cfg.getInitParameter("param1");
+		String p2 = cfg.getInitParameter("param2");
+		String servletName = cfg.getServletName();
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>Annotation方式配置Servlet测试</h1>" + "<br/>");
+		out.println("<hr>");
+		out.println(servletName + "的param1: " + p1 + "<br/>");
+		out.println(servletName + "的param2: " + p2 + "<br/>");
+	}
+}
