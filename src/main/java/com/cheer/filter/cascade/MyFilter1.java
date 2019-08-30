4524325da11ab46b5d1fe16f985e7768f2de7a83
yinchu <javayinchu2013@163.com>
2019-08-30 12:04:55 +0800

    updated

---
+++ src/main/java/com/cheer/filter/cascade/MyFilter1.java
@@ -0,0 +1,29 @@
+package com.cheer.filter.cascade;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyFilter1 implements Filter {
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("执行MyFilter1中的doFilter中...");
+		chain.doFilter(request, response);
+		System.out.println("执行MyFilter1中的doFilter完毕...");
+	}
+
+	public void destroy() {
+
+	}
+
+}
---
+++ src/main/java/com/cheer/filter/cascade/MyServlet.java
@@ -0,0 +1,19 @@
+package com.cheer.filter.cascade;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>测试多个过滤器串联执行顺序!</h1>");
+	}
+}
